a6b7158571488705ad7c5a7801f7552b388de9a7
taha-sindi <taha.sindi@gmail.com>
2019-06-19 12:23:46 +0300

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 protected
 def configure_permitted_parameters
 devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :location, :phone])
-end end
+end
+def after_sign_in_path_for(resource)
+"/lists"end end
